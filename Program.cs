b0c8a937623b373259a62a60c662c1a2ad176607
One <One@DESKTOP-MIB22JC>
2022-05-03 17:54:21 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Random random = new Random();
+        byte minimumCostForBook = 1;
+        byte maximumCostForBook = 28;
+        int maximumGoldForTrade = 1000;
+        int minimumGoldForTrade = 100;
+        int countBookForTrade = 0;
+        int randomCostBook = random.Next(minimumCostForBook, maximumCostForBook);
+
+        for (int i = 0; i <= maximumGoldForTrade; i += randomCostBook)
+        {
+            if (i >= minimumGoldForTrade)
+            {
+                countBookForTrade++;
+            }
+        }
+        Console.WriteLine(countBookForTrade);
+    }
+}
